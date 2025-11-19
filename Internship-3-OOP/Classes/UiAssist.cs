650ad2736d40cbf75fa5489aef2bc2e3eab438f8
Luka K. Mihovilović <lukak.business@gmail.com>
2025-11-19 17:09:23 +0100

    Add old UI code and adapt it, add main menu placeholders

---
+++ Internship-3-OOP/Classes/UiAssist.cs
@@ -0,0 +1,101 @@
+using System.Runtime.InteropServices;
+
+namespace Internship_3_OOP.Classes;
+
+public class UiAssist
+{
+    private const string Title = "APLIKACIJA ZA UPRAVLJANJE AERODROMOM";
+    
+    public static void Halt()
+    {
+        Console.Write("Pritisnite bilo koju tipku za povratak...");
+        Console.ReadKey();
+    }
+
+    public static void ShowFullscreenMessage(string message)
+    {
+        Console.Clear();
+        Console.Write("{0}\n\n{1}\n\n", Title, message);
+        Halt();
+    }
+    
+    public static int PromptMenu(Tuple<string, Action>[] options, string? subtitle = null)
+    {
+        Console.Clear();
+
+        Console.WriteLine("{0}\n", Title);
+        if (subtitle is not null)
+            Console.WriteLine("{0}\n", subtitle);
+
+        for (var i = 0; i < options.Length; i++)
+        {
+            Console.WriteLine(
+                $"{(i != options.Length - 1 ? i + 1 : 0)} - {options[i].Item1}"
+            );
+        }
+
+        Console.WriteLine();
+        var choice = OneLinePromptIntInRange("Odabir: ", -1, options.Length);
+        options[(choice == 0 ? options.Length : choice) - 1].Item2();
+        return choice;
+    }
+
+    private static void BringCursorBackToPrompt(int promptLength, int userInputLength)
+    {
+        for (var i = 0; i < (promptLength + userInputLength) / Console.BufferWidth + 1; i++)
+            Console.CursorTop--;
+        Console.CursorLeft = promptLength;
+        var savedPos = Console.GetCursorPosition();
+        Console.Write(new string(' ', userInputLength));
+        Console.SetCursorPosition(savedPos.Left, savedPos.Top);
+
+        var invalidInputWarning = "Nevažeći unos!";
+        Console.Write("\a\x1b[31mNevažeći unos!\x1b[0m");
+        Thread.Sleep(1500);
+        Console.Write(new string('\b', invalidInputWarning.Length));
+        Console.Write(new string(' ', invalidInputWarning.Length));
+        Console.Write(new string('\b', invalidInputWarning.Length));
+    }
+
+    public static int OneLinePromptIntInRange(string prompt, int lower, int higher)
+    {
+        Console.Write(prompt);
+        var firstLoop = true;
+        var lastEnteredLength = 0;
+        while (true)
+        {
+            if (!firstLoop)
+                BringCursorBackToPrompt(prompt.Length, lastEnteredLength);
+            else
+                firstLoop = false;
+
+            var inputted = Console.ReadLine();
+            if (inputted is null)
+                continue;
+            inputted = inputted.Trim();
+            lastEnteredLength = inputted.Length;
+
+            if (!int.TryParse(inputted, out var parsed)) continue;
+            if (parsed > lower && parsed < higher) return parsed;
+        }
+    }
+    
+    public static string OneLinePromptString(string prompt)
+    {
+        Console.Write(prompt);
+        var isFirstLoop = true;
+        while (true)
+        {
+            if (!isFirstLoop)
+                BringCursorBackToPrompt(prompt.Length, 0);
+            else
+                isFirstLoop = false;
+
+            var inputted = Console.ReadLine();
+            if (inputted is null) continue;
+            inputted = inputted.Trim();
+
+            return inputted;
+        }
+    }
+}
---
+++ Internship-3-OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using Internship_3_OOP.Classes;
+
+namespace Internship_3_OOP;
+
+class Program
+{
+    private static void Main()
+    {
+        while (true)
+        {
+            UiAssist.PromptMenu([
+                new Tuple<string, Action>(
+                    "Putnici", () => { UiAssist.ShowFullscreenMessage("PLACEHOLDER - putnici"); }
+                ),
+                new Tuple<string, Action>(
+                    "Letovi", () => { UiAssist.ShowFullscreenMessage("PLACEHOLDER - letovi"); }
+                ),
+                new Tuple<string, Action>(
+                    "Avioni", () => { UiAssist.ShowFullscreenMessage("PLACEHOLDER - avioni"); }
+                ),
+                new Tuple<string, Action>(
+                    "Posada", () => { UiAssist.ShowFullscreenMessage("PLACEHOLDER - posada"); }
+                ),
+                new Tuple<string, Action>(
+                    "Izlaz iz programa", () => { Environment.Exit(0); }
+                )
+            ]);
+        }
+    }
+}
