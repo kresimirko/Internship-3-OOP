01dc14b04507c7d15e67480aaed62bfc9fc6867c
Luka K. Mihovilović <lukak.business@gmail.com>
2025-11-22 13:58:40 +0100

    Flight addition/deletion (no time in DateTime yet)

--- Internship-3-OOP/Menus/MenuFlights.cs
+++ Internship-3-OOP/Menus/MenuFlights.cs
@@ -14,7 +14,94 @@ public static class MenuFlights
 
     private static void AddFlight()
     {
-        UiAssist.ClearAndPrintAppHeader("Dodavanje leta");
+        UiAssist.ClearAndPrintAppHeader("Dodavanje novog leta");
+        
+        var name = UiAssist.OneLinePrompt<string>("Naziv: ");
+        var departure = UiAssist.OneLinePrompt<DateTime>("Vrijeme polaska: ");
+        var departureLocation = UiAssist.OneLinePrompt<string>("Mjesto polaska: ");
+        var arrival = UiAssist.OneLinePrompt<DateTime>("Vrijeme dolaska: ");
+        var arrivalLocation = UiAssist.OneLinePrompt<string>("Mjesto dolaska: ");
+        var distance = UiAssist.OneLinePromptIntInRange(19, 1001, "Udaljenost: ");
+
+        if (Storage.Flights.Any(flight => name == flight.Name))
+        {
+            Console.WriteLine("Let s ovim imenom već postoji.");
+            UiAssist.Halt();
+            return;
+        }
+        
+        Console.Write("Avion: [pritisnite Enter]");
+        Console.ReadKey();
+        var airplane = UiAssist.PromptMenu(
+            Storage.Airplanes.Members.Select(airplane => airplane.Name).ToArray(), "Avion");
+        
+        Console.Write("Posada: [pritisnite Enter]");
+        Console.ReadKey();
+        var aircrew = UiAssist.PromptMenu(
+            Storage.AircrewGroup.Members.Select(aircrew => aircrew.Name).ToArray(), "Posada");
+        
+        if (!UiAssist.PromptYesNoChoice("Dodavanje leta uspješno.", "Dodavanje leta otkazano.",
+                $"Jeste li sigurni da želite dodati let \"{name}\"?"))
+            return;
+
+        Storage.Flights.Add(new EntityFlight(name, departure, departureLocation, arrival, arrivalLocation, distance));
+            
+        UiAssist.Halt();
+    }
+    
+    private static void DeleteFlight()
+    {
+        var choice = UiAssist.PromptMenu([
+            "Po ID-u",
+            "Po nazivu"
+        ], "Brisanje aviona");
+
+        EntityFlight[] selected;
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Molimo vas upišite puni ID. Evo svih letova:");
+                Storage.Flights.PrintDataTable();
+                
+                var id = UiAssist.OneLinePrompt<string>("Upišite ID leta: ").ToLower();
+                
+                selected = (from flight in Storage.Flights where UiAssist.GetShortGuidString(flight.Guid) == id.ToLower() select flight).ToArray();
+                
+                if (selected.Length == 0)
+                {
+                    Console.WriteLine("Nema leta s ovim ID-om.");
+                    UiAssist.Halt();
+                    return;
+                }
+                
+                if (!UiAssist.PromptYesNoChoice("Brisanje leta uspješno.", "Brisanje leta otkazano.",
+                        $"Jeste li sigurni da želite izbrisati let \"{selected[0].Name}\"?"))
+                    return;
+
+                Storage.Flights.Remove(selected[0]);
+                break;
+            case 0:
+                Console.WriteLine("Molimo vas upišite puni naziv. Evo svih aviona:");
+                Storage.Flights.PrintDataTable();
+                
+                var name = UiAssist.OneLinePrompt<string>("Upišite ime aviona: ").ToLower();
+                
+                selected = (from flight in Storage.Flights where flight.Name.ToLower() == name.ToLower() select flight).ToArray();
+                
+                if (selected.Length == 0)
+                {
+                    Console.WriteLine("Nema aviona s ovim imenom.");
+                    UiAssist.Halt();
+                    return;
+                }
+                
+                if (!UiAssist.PromptYesNoChoice("Brisanje leta uspješno.", "Brisanje leta otkazano.",
+                        $"Jeste li sigurni da želite izbrisati let \"{selected[0].Name}\"?"))
+                    return;
+
+                Storage.Flights.Remove(selected[0]);
+                break;
+        }
         
         UiAssist.Halt();
     }
@@ -33,13 +120,6 @@ public static class MenuFlights
     private static void EditFlight()
     {
         UiAssist.ClearAndPrintAppHeader("Uređivanje leta");
-        
-        UiAssist.Halt();
-    }
-
-    private static void DeleteFlight()
-    {
-        UiAssist.ClearAndPrintAppHeader("Brisanje leta");
         
         UiAssist.Halt();
     }
